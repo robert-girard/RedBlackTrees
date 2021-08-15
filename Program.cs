3b6a2bb9df793e1cb20921f3f279bf161299d8c0
robert-girard <r_girard@live.com>
2021-08-15 14:32:21 -0400

    Make the Tree Enumerable

--- Program.cs
+++ Program.cs
@@ -16,9 +16,19 @@ namespace RedBlackTrees
             myTree.addNode(200);
             myTree.addNode(50);
             myTree.addNode(75);
+            myTree.addNode(750);
+            myTree.addNode(7500);
+            myTree.addNode(75000);
             List<int> inOrder = myTree.inOrderTransversal();
 
+            Console.WriteLine("Creating List: ");
             foreach (int i in inOrder)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("uisng IEnumerable: ");
+            foreach (int i in myTree)
             {
                 Console.WriteLine(i);
             }
--- RedBlackTree.cs
+++ RedBlackTree.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace RedBlackTrees
 {
-    class RedBlackTree<T>
+    class RedBlackTree<T> : IEnumerable<T>
     {
         /* RadBlackTree Rules
          * 
@@ -261,5 +262,24 @@ namespace RedBlackTrees
             return ordered;
         }
 
+        
+        public IEnumerator<T> GetEnumerator()
+        {
+            if (this.root == null)
+            {
+                yield break;
+            }
+
+            foreach (T i in this.inOrderTransversal())
+            {
+                yield return i;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+        
     }
 }
