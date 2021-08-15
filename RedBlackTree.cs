2e34c20e0abb502cc3ddc22321645573b92b2670
robert-girard <r_girard@live.com>
2021-08-14 21:42:57 -0400

    Added RedBlackTees class outline

---
+++ RedBlackTree.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedBlackTrees
+{
+    class RedBlackTree<T>
+    {
+        private class Node<T>
+        {
+            private Node<T> leftChild;
+            private Node<T> rightChild;
+            private T value;
+            private bool colour;
+
+            public Node(T value)
+            {
+                this.value = value;
+                this.colour = true;
+                this.leftChild = null;
+                this.rightChild = null;
+            }
+        }
+
+        private Node<T> root;
+
+        public RedBlackTree()
+        {
+            this.root = null;
+        }
+
+        public void addNode(T value)
+        {
+            
+        }
+
+        public void deleteNode(T value)
+        {
+
+        }
+
+        public List<T> DFS()
+        {
+            return new List<T>();
+        }
+
+        public List<T> BFS()
+        {
+            return new List<T>();
+        }
+
+        public List<T> inOrderTransversal()
+        {
+            return new List<T>();
+        }
+
+    }
+}
