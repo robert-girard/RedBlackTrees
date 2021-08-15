14e5389d949a8bafe0ba746c9ccc927177d20bd9
robert-girard <r_girard@live.com>
2021-08-15 10:23:07 -0400

    Added exception for inserted value aready in tree and partial implementation of insert

--- RedBlackTree.cs
+++ RedBlackTree.cs
@@ -10,17 +10,27 @@ namespace RedBlackTrees
     {
         private class Node<T>
         {
-            private Node<T> leftChild;
-            private Node<T> rightChild;
-            private T value;
-            private bool colour;
+            public Node<T> parent;
+            public Node<T> left;
+            public Node<T> right;
+            public T value;
+            public bool colour;
 
             public Node(T value)
             {
                 this.value = value;
                 this.colour = true;
-                this.leftChild = null;
-                this.rightChild = null;
+                this.left = null;
+                this.right = null;
+                this.parent = null;
+            }
+            public Node(T value, Node<T> parent)
+            {
+                this.value = value;
+                this.colour = true;
+                this.left = null;
+                this.right = null;
+                this.parent = parent;
             }
         }
 
@@ -31,9 +41,51 @@ namespace RedBlackTrees
             this.root = null;
         }
 
+        private void cases(Node<T> node)
+        {
+
+        }
+
+        private void insert(Node<T> node, T value)
+        {
+            int comparison = Comparer<T>.Default.Compare(node.value, value);                                //for generic comparisons, returns 0 if ==, -ve if <, or +ve if >
+            if (comparison == 0)
+            {
+                throw new DuplicateValueException(string.Format("Value: {0} already in tree", value));
+            } else if (comparison < 0)
+            {
+                if (node.left == null)
+                {
+                    node.left = new Node<T>(value, node);
+                    this.cases(node.left);
+                } else
+                {
+                    this.insert(node.left, value);
+                }
+            } else if (comparison > 0)
+            {
+                if (node.right == null)
+                {
+                    node.right = new Node<T>(value, node);
+                    this.cases(node.right);
+                }
+                else
+                {
+                    this.insert(node.right, value);
+                }
+            }
+
+        }
+
         public void addNode(T value)
         {
-            
+            if (this.root == null)
+            {
+                this.root = new Node<T>(value);
+                return;
+            }
+
+            this.insert(this.root, value);   
         }
 
         public void deleteNode(T value)
---
+++ RedBlackTreeExceptions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+[Serializable]
+public class DuplicateValueException : Exception
+{
+    // Constructors
+    public DuplicateValueException(string message)
+        : base(message)
+    { }
+}
